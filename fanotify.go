a1409c36ddf8e0f344b6d2df7ad8c05ecd7a90d4
Giannis Zach <gianniszach@users.noreply.github.com>
2023-05-17 15:31:37 +0300

    Update fanotify.go
    - added more mask marks 
    - replaced all `panic()`'s with returned errors
    - introduced `isFiltered()` function as an argument

--- fanotify.go
+++ fanotify.go
@@ -1,8 +1,7 @@
 // Modified version of https://github.com/ozeidan/gosearch/blob/master/internal/fanotify/fanotify.go
 // Changes:
 // - support for modified files
-// 
-//
+// - support for attributes change
 package prsdmfanotify
 
 import (
@@ -20,23 +19,26 @@ import (
 )
 
 const (
-	fanReportFid      = 0x00000200
-	fanMarkAdd        = 0x00000001
-	fanMarkFilesystem = 0x00000100
-	fanOndir          = 0x40000000 /* event occurred against dir */
-	fanMovedFrom      = 0x00000040 /* File was moved from X */
-	fanMovedTo        = 0x00000080 /* File was moved to Y */
-	fanModify         = 0x00000002 /* File was modified */
-	fanCreate         = 0x00000100 /* Subfile was created */
-	fanDelete         = 0x00000200 /* Subfile was deleted */
-	fanDeleteSelf     = 0x00000400 /* Self was deleted */
-	fanMoveSelf       = 0x00000800 /* Self was moved */	
-	fanEventOnChild   = 0x08000000 /* interested in child events */
-	atFDCWD           = -100
+	fanReportFid        = 0x00000200
+	fanMarkAdd          = 0x00000001
+	fanMarkFilesystem   = 0x00000100
+	fanOndir            = 0x40000000 /* event occurred against dir */
+	fanMovedFrom        = 0x00000040 /* File was moved from X */
+	fanMovedTo          = 0x00000080 /* File was moved to Y */
+	fanModify           = 0x00000002 /* File was modified */
+	fanAttrib           = 0x00000004 /* Metadata changed */
+	fanCloseWrite       = 0x00000008 /* File closed with write */
+	fanCreate           = 0x00000100 /* Subfile was created */
+	fanDelete           = 0x00000200 /* Subfile was deleted */
+	fanDeleteSelf       = 0x00000400 /* Self was deleted */
+	fanMoveSelf         = 0x00000800 /* Self was moved */
+	fanEventOnChild     = 0x08000000 /* interested in child events */
+	atFDCWD             = -100
 	fanEventInfoTypeFid = 1 /* FAN_EVENT_INFO_TYPE_FID */
+	fanMarkOnlyDir      = 0x00000008
 )
 const markFlags = fanMarkAdd | fanMarkFilesystem
-const markMask = fanOndir | fanMovedFrom | fanMovedTo | fanCreate | fanDelete | fanModify
+const markMask = fanOndir | fanMovedFrom | fanMovedTo | fanCreate | fanCloseWrite | fanDelete | fanModify | fanAttrib | fanEventOnChild
 
 type fanotifyInfoHeader struct {
 	infoType uint8
@@ -82,36 +84,42 @@ const (
 func Listen(
 	listenDir string,
 	isFiltered func(path string) bool,
-	changeReceiver chan<- FileChange
-) error {	
+	changeReceiver chan<- FileChange,
+) error {
 	fan, err := unix.FanotifyInit(fanReportFid, 0)
+	//fan, err := unix.FanotifyInit(unix.FAN_CLASS_NOTIF|unix.FAN_REPORT_DFID_NAME, 0)
 	if err != nil {
 		return fmt.Errorf("Error unix.FanotifyInit(): %v", err)
 	}
 
 	err = unix.FanotifyMark(fan, markFlags, markMask, atFDCWD, listenDir)
+  	//err = unix.FanotifyMark(fan,
+	//	unix.FAN_MARK_ADD|fanMarkOnlyDir,
+	//	unix.FAN_CREATE|unix.FAN_ONDIR,
+	//	atFDCWD, listenDir)
 
 	if err != nil {
 		return fmt.Errorf("Error unix.FanotifyMark(): %v", err)
 	}
 
-	log.Println("starting to listen on fanotify events")
+	log.Println("start listening for fanotify events")
 
 	f := os.NewFile(uintptr(fan), "")
 	r := bufio.NewReader(f)
 
 	for {
-		readEvent(r, changeReceiver)
+		readEvent(r, isFiltered, changeReceiver)
 	}
 }
 
 var metaBuff = make([]byte, 24)
 
 func readEvent(
-	r io.Reader, 
-	isFiltered func(path string) bool, 
-	changeReceiver chan<- FileChange
+	r io.Reader,
+	isFiltered func(path string) bool,
+	changeReceiver chan<- FileChange,
 ) error {
+	//log.Println("Got event")
 	n, err := r.Read(metaBuff)
 	if err != nil {
 		return fmt.Errorf("Error reading metaBuff: %v", err)
@@ -121,6 +129,7 @@ func readEvent(
 		return fmt.Errorf("Error reading metaBuff out of range: %v", err)
 	}
 
+  	//extract event's metadata in infoBuff
 	meta := *((*unix.FanotifyEventMetadata)(unsafe.Pointer(&metaBuff[0])))
 	bytesLeft := int(meta.Event_len - uint32(meta.Metadata_len))
 	infoBuff := make([]byte, bytesLeft)
@@ -133,12 +142,15 @@ func readEvent(
 		return fmt.Errorf("Error reading infoBuff out of range: %v", err)
 	}
 
+  	//populate info fanotifyEventInfoFid from infoBuff
 	info := *((*fanotifyEventInfoFid)(unsafe.Pointer(&infoBuff[0])))
 
+  	//check the infotype, we only support event info type fid
 	if info.hdr.infoType != fanEventInfoTypeFid {
 		return nil
 	}
 
+  	//find the filedescriptor from info's filehandle
 	handleStart := uint32(unsafe.Sizeof(info))
 	handleLen := info.eventFid.fileHandle.handleBytes
 	handleBytes := infoBuff[handleStart : handleStart+handleLen]
@@ -146,7 +158,7 @@ func readEvent(
 
 	fd, err := unix.OpenByHandleAt(atFDCWD, unixFileHandle, 0)
 	if err != nil {
-		return fmt.Errorf("Error could not call OpenByHandleAt: %v", err)		
+		return fmt.Errorf("Error could not call OpenByHandleAt: %v", err)
 	}
 
 	defer func() {
@@ -162,12 +174,11 @@ func readEvent(
 
 	if err != nil {
 		return fmt.Errorf("Error could not call Readlink: %v", err)
-		return
 	}
 	path = path[:pathLength]
 
 	if isFiltered(string(path)) {
-	 	return nil
+		return nil
 	}
 
 	log.Println("received event, path:", string(path),
@@ -189,6 +200,7 @@ func readEvent(
 	}
 
 	changeReceiver <- change
+	return nil
 }
 
 func maskToString(mask uint64) string {
